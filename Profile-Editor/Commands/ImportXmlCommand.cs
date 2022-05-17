50b239f487b7dc2af6c5569b59e9ff4be44c912b
Nils Bezjak <nils.bezjak@gmail.com>
2022-05-17 13:15:55 +0200

    int: open and read xml file

---
+++ Profile-Editor/Commands/ImportXmlCommand.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Profile_Editor.Commands
+{
+    internal class ImportXmlCommand : ICommand
+    {
+         
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            string filepath = "";
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.InitialDirectory = Environment.CurrentDirectory;
+            fd.Filter = "xml files (*.xml)|*.xml";
+            fd.RestoreDirectory = true;
+
+            if (fd.ShowDialog() == true)
+            {
+                filepath = fd.FileName;
+                if (string.IsNullOrEmpty(filepath))
+                {
+                    return;
+                }
+            }
+
+            DataSet ds = new DataSet();
+            ds.ReadXml(filepath);
+            DataView bla = ds.Tables[0].DefaultView;
+        }
+    }
+}
---
+++ Profile-Editor/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Profile_Editor.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Profile_Editor.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        public ICommand ImportXmlCommand { get; set; }
+
+        public MainViewModel()
+        {
+            ImportXmlCommand = new ImportXmlCommand();
+        }
+    }
+}
