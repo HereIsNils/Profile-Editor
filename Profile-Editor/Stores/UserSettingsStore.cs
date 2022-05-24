2d223b7b5b2bb76e89ba8bb461aa7a8fb3bb9d63
Nils Bezjak <nils.bezjak@gmail.com>
2022-05-24 15:52:46 +0200

    feat: add stores for UserSettings

---
+++ Profile-Editor/Stores/UserSettingsStore.cs
@@ -0,0 +1,29 @@
+﻿using Profile_Editor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile_Editor.Stores
+{
+    internal class UserSettingsStore
+    {
+        private UserSettings _userSettings;
+        public UserSettings userSettings => _userSettings;
+
+        public event Action<UserSettings> UserSettingsCreated;
+
+        public UserSettingsStore()
+        {
+
+            _userSettings = new UserSettings();
+        }
+
+        public void CreateUserSettings(UserSettings settings)
+        {
+            if(settings == null) return;
+            _userSettings = settings;
+        }
+    }
+}
