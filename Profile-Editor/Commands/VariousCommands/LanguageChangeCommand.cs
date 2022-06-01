e9754f304209f25a7f3cb7a5d6debcc7d4d824e4
Nils Bezjak <nils.bezjak@gmail.com>
2022-06-01 14:58:26 +0200

    feat: implement interaction logic for VViewModel

---
+++ Profile-Editor/Commands/VariousCommands/LanguageChangeCommand.cs
@@ -0,0 +1,35 @@
+﻿using Profile_Editor.Stores;
+using Profile_Editor.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Profile_Editor.Commands.VariousCommands
+{
+    internal class LanguageChangeCommand : ICommand
+    {
+        private UserSettingsStore userSettingsStore { get; }
+        private VViewModel vViewModel { get; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public LanguageChangeCommand(UserSettingsStore userSettingsStore, VViewModel vViewModel)
+        {
+            this.userSettingsStore = userSettingsStore;
+            this.vViewModel = vViewModel;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            userSettingsStore.userSettings.Various[0].Language = parameter.ToString();
+        }
+    }
+}
