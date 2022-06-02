e476c6ffe4fcaa528b11bc5c92f665e415ead3b4
Nils Bezjak <nils.bezjak@gmail.com>
2022-06-02 10:50:33 +0200

    int: create fields and set bindings

--- Profile-Editor/ViewModel/IViewModel.cs
+++ Profile-Editor/ViewModel/IViewModel.cs
@@ -1,18 +1,146 @@
-﻿using System;
+﻿using Profile_Editor.Commands.InstrumentCommands;
+using Profile_Editor.Model;
+using Profile_Editor.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Profile_Editor.ViewModel
 {
     internal class IViewModel : BaseViewModel
     {
-        public string AppLevel { get; set; }
-
-        public IViewModel()
+        private UserSettingsStore _userSettingsStore;
+        public UserSettingsStore UserSettingsStore
         {
-            AppLevel = "App Level: 1";
+            get { return _userSettingsStore; }
+            set
+            {
+                _userSettingsStore = value;
+                OnPropertyChanged(nameof(_userSettingsStore));
+            }
+        }
+
+        public UserSettings userSettings { get; set; }
+
+        #region Commands
+        public ICommand HolderChangedCommand { get; set; }
+        public ICommand SCenterValueChangedCommand { get; set; }
+        public ICommand RotationChangedCommand { get; set; }
+        public ICommand AuxChangedCommand { get; set; }
+        public ICommand CoolantStateChangedCommand { get; set; }
+        public ICommand NaclStateChangedCommand { get; set; }
+        public ICommand AircoolingChangedCommand { get; set; }
+        public ICommand AirWatercoolingChangedCommand { get; set; }
+        public ICommand NaclcoolingChangedCommand { get; set; }
+        public ICommand LuxStateChangedCommand { get; set; }
+        public ICommand LuxLevelChangedCommand { get; set; }
+        #endregion Commands
+
+        #region IViewModel Members
+        private int _HolderIndex;
+        public int HolderIndex
+        {
+            get { return _HolderIndex; }
+            set { _HolderIndex = value; OnPropertyChanged(nameof(HolderIndex)); }
+        }
+
+        private string _AppLevel;
+        public string AppLevel
+        {
+            get { return _AppLevel; }
+            set { _AppLevel = value; OnPropertyChanged(nameof(AppLevel)); }
+        } // sets app level text
+
+        private int _ITypeIndex;
+        public int ITypeIndex
+        {
+            get { return _ITypeIndex; }
+            set { _ITypeIndex = value; OnPropertyChanged(nameof(ITypeIndex)); }
+        }// index of intrument type combobox
+
+        private int _SCenterValue;
+        public int SCenterValue
+        {
+            get { return _SCenterValue; }
+            set { _SCenterValue = value; OnPropertyChanged(nameof(SCenterValue)); }
+        } // value of pedal slider
+
+        private int _AuxIndex;
+        public int AuxIndex
+        {
+            get { return _AuxIndex; }
+            set { _AuxIndex = value; OnPropertyChanged(nameof(AuxIndex)); }
+        } // index of Aux combobox
+
+        private bool _CoolantState;
+        public bool CoolantState
+        {
+            get { return _CoolantState; }
+            set { _CoolantState = value; OnPropertyChanged(nameof(CoolantState)); }
+        } // cooling mode enabled or disabled
+
+        private bool _NaclState;
+        public bool NaclState
+        {
+            get { return _NaclState; }
+            set { _NaclState = value; OnPropertyChanged(nameof(NaclState)); }
+        } // NaCl enabled or disabled
+
+        private bool _Aircooling;
+        public bool Aircooling
+        {
+            get { return _Aircooling; }
+            set { _Aircooling = value; OnPropertyChanged(nameof(Aircooling)); }
+        } // enable or disable aircooling
+
+        private bool _AirWatercooling;
+        public bool AirWatercooling
+        {
+            get { return _AirWatercooling; }
+            set { _AirWatercooling = value; OnPropertyChanged(nameof(AirWatercooling)); }
+        } //  enable or disable air and water cooling
+
+        private bool _NaclCooling;
+        public bool Naclcooling
+        {
+            get { return _NaclCooling; }
+            set { _NaclCooling = value; OnPropertyChanged(nameof(_NaclCooling)); }
+        } //  enable or disable NaCl cooling
+
+        private bool _LuxState;
+        public bool LuxState
+        {
+            get { return _LuxState; }
+            set { _LuxState = value; OnPropertyChanged(nameof(LuxState)); }
+        } //  enable or disable lux
+
+        private int _LuxLevel;
+        public int LuxLevel
+        {
+            get { return _LuxLevel; }
+            set { _LuxLevel = value; OnPropertyChanged(nameof(LuxLevel)); }
+        } // value for the lux level
+        #endregion IViewModel Members
+
+        public IViewModel(UserSettingsStore userSettingsStore, UserSettings userSettings)
+        {
+            HolderChangedCommand = new HolderChangedCommand();
+            SCenterValueChangedCommand = new SCenterValueChangedCommand();
+            RotationChangedCommand = new RotationChangedCommand();
+            AuxChangedCommand = new AuxChangedCommand();
+            CoolantStateChangedCommand = new CoolantStateChangedCommand();
+            NaclStateChangedCommand = new NaclStateChangedCommand();
+            AircoolingChangedCommand = new AircoolingChangedCommand();
+            AirWatercoolingChangedCommand = new AirWaterCoolingChangedCommand();
+            NaclcoolingChangedCommand = new NaclcoolingChangedCommand();
+            LuxStateChangedCommand = new LuxStateChangedCommand();
+            LuxLevelChangedCommand = new LuxLevelChangedCommand();
+
+            _userSettingsStore = userSettingsStore;
+            this.userSettings = userSettings;
         }
     }
 }
