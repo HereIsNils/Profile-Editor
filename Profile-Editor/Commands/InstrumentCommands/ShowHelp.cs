4ed07e4f6df5e7c01849a27c941c4703255da1c0
Nils Bezjak <nils.bezjak@gmail.com>
2022-06-29 09:37:30 +0200

    feat: update help button

--- Profile-Editor/Commands/InstrumentCommands/ShowHelp.cs
+++ Profile-Editor/Commands/InstrumentCommands/ShowHelp.cs
@@ -23,7 +23,13 @@ namespace Profile_Editor.Commands.InstrumentCommands
                 "Weitere Informationen dazu finden Sie in der Kurzanleitung Profil-Editor.\n" +
                 "\nDer Halter, welcher konfiguriert werden soll, wird mit der Combobox festgelegt. Der an der Einheit angeschlossene Instrumententyp" +
                 "wird dabei ignoriert.\n" +
-                "\nDie Fußpedalmitte wird über den Slider festgelegt. Der Slider hat einen Minimalwert von 1 und einen Maximalwert von 128.";
+                "\nDie Fußpedalmitte wird über den Slider festgelegt. Der Slider hat einen Minimalwert von 1 und einen Maximalwert von 128." +
+                "1 entspricht der linken Seite und 128 der rechten Seite des Fußpedals.\n" +
+                "\nDie Rotationsrichtung kann mit der Combobox festgelegt werden.\n" +
+                "\nDer Modus für die Beschleunigung kann über die Combobox festgelegt werden.\n" +
+                "\nDer Kühlmodus kann ausgewählt werden, sobald der Haken bei Kühlung gesetzt wurde. NaCl kann ausgewählt werden, " +
+                "wenn der Haken bei NaCl aktiv gesetzt wurde. Es kann nur ein Kühlmodus gleichzeitig ausgewählt werden.\n" +
+                "\nDas Level der Helligkeit kann festgelegt werden, sobald der Haken bei Helligkeit gesetzt wurde.";
             MessageBox.Show(text);
         }
     }
